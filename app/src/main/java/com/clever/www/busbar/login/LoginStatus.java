b218edf76cec60f41bdd1278e05297f3f1ec150c
lzy <luozhiyong131@qq.com>
2017-08-02 10:33:31 +0800

    通讯正常

---
+++ app/src/main/java/com/clever/www/busbar/login/LoginStatus.java
@@ -0,0 +1,43 @@
+package com.clever.www.busbar.login;
+
+
+import com.clever.www.busbar.dp.data.hash.read.BusHashRead;
+import com.clever.www.busbar.dp.data.packages.DevDataPacket;
+
+/**
+ * Author: lzy. Created on: 16-11-3.
+ */
+public class LoginStatus {
+    public static boolean isLogin = false; // 默认为非连接
+    public static String login_ip;
+    public static int login_devNum = 0;
+    public static String login_usr;
+    public static String login_pwd;
+    public static boolean isRemember = false;
+
+
+    private static BusHashRead mReadHash = new BusHashRead();
+    public static DevDataPacket getPacket(int num) {
+        DevDataPacket dataPacket = mReadHash.get(login_devNum, num);;
+        if(!isLogin) {
+            if(dataPacket != null)
+                dataPacket.offLine = 0;
+        }
+        return dataPacket;
+    }
+
+    public static boolean getLogin() {
+        if(isLogin) {
+            DevDataPacket dataPacket = getPacket(0);
+            if(dataPacket != null) {
+                if(dataPacket.offLine == 0)
+                    isLogin = false;
+            } else {
+                isLogin = false;
+            }
+        }
+
+        return isLogin;
+    }
+
+}
--- app/src/main/java/com/clever/www/busbar/net/udp/sent/UdpHeartbeat.java
+++ app/src/main/java/com/clever/www/busbar/net/udp/sent/UdpHeartbeat.java
@@ -6,27 +6,46 @@ package com.clever.www.busbar.net.udp.sent;
 
 public class UdpHeartbeat {
     private static final int PORT = 8080;
-    private static final String MSG = "Clever-Manager PDU PC Server OK!";
-    UdpSend udp = new UdpSend();
+    private static final String MSG = "Clever-Manager PDU APP Server OK!";
+    private UdpSend udp = new UdpSend();
+    private static UdpHeartbeat mUdpHeartbeat = null;
+    private boolean isRun = false;
+    private String mIpAddr;
 
-    public void start() {
+    public static UdpHeartbeat bulid() {
+        if(mUdpHeartbeat == null) {
+            mUdpHeartbeat = new UdpHeartbeat();
+        }
+
+        return mUdpHeartbeat;
+    }
+
+    public void startThread(String ip) {
+        mIpAddr = ip;
+        isRun = true;
         sentThread();
+    }
+
+    public void stopThread() {
+        isRun = false;
     }
 
     private void sentThread() {
         new Thread() {
             @Override
             public void run() {
-                while (true)
+                while (isRun) {
                     sent();
+                }
             }
         }.start();
     }
 
     private void sent() {
         try {
+            udp.sent(mIpAddr, PORT, MSG.getBytes(), MSG.length());
+//            udp.dbSent(PORT, MSG.getBytes(), MSG.length());
             Thread.sleep(1000);
-            udp.dbSent(PORT, MSG.getBytes(), MSG.length());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
